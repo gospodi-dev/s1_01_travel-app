23defe20050b8e6425319bdeb8bd9bf280aa9e87
Gospodi <gospodijazzet@yandex.ru>
2021-10-23 16:58:07 +0300

    class TravelHomePage

--- flutter_travel_app_s1_01/lib/main.dart
+++ flutter_travel_app_s1_01/lib/main.dart
@@ -7,6 +7,21 @@ void main() {
 class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Flutter Travel App (S1_01)',
+      home: TravelHomePage(),
+    );
+  }
+}
+
+class TravelHomePage extends StatefulWidget {
+  @override
+  _TravelHomePageState createState() => _TravelHomePageState();
+}
+
+class _TravelHomePageState extends State<TravelHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
